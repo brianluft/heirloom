e41f524dd537a6cb0ddddae182634df339bd9125
Brian Luft <brian@electroly.com>
2026-02-26 00:52:08 -0400

    Convert File Manager window icons to SVG

--- src/winfile/images/app_ico_create.bat
+++
@@ -1,1 +0,0 @@
-magick app16.png app32.png app48.png app64.png app128.png app256.png app512.png app.ico
---
+++ src/winfile/images/generate_icons.bat
@@ -0,0 +1,8 @@
+del app.ico
+magick app16.png app32.png app48.png app64.png app128.png app256.png app512.png app.ico
+del wftree.ico
+magick wftree16.png wftree32.png wftree48.png wftree64.png wftree128.png wftree256.png wftree512.png wftree.ico
+del wfdir.ico
+magick wfdir16.png wfdir32.png wfdir48.png wfdir64.png wfdir128.png wfdir256.png wfdir512.png wfdir.ico
+del wftrdir.ico
+magick wftrdir16.png wftrdir32.png wftrdir48.png wftrdir64.png wftrdir128.png wftrdir256.png wftrdir512.png wftrdir.ico
